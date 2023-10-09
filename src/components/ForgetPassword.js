28bd0d466f015dea2bb8e41434e927f64acaa5e7
Aswini-mca <aswini25.mca@gmail.com>
2023-10-09 10:00:40 +0530

    updated API

--- src/components/ForgetPassword.js
+++ src/components/ForgetPassword.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { Link,useNavigate} from 'react-router-dom'
-import { API } from './global'
+import { API } from '../global'
 
 //forgetpassword component
 function ForgetPassword() {
